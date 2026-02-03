7b002f4f451d3dad156cf3f7b5ed1c279ff011a2
Jimpixx17 <135128863+Jimpixx17@users.noreply.github.com>
2026-02-03 10:04:35 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [yes, setYes] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      {yes ? (
+        <h1>
+          💖 Yaaay!! 💖 <br />
+          Ich freu mich 😍
+        </h1>
+      ) : (
+        <>
+          <h1>
+            Willst du mein <br />
+            Valentinstag-Date sein? 💘
+          </h1>
+
+          <div className="buttons">
+            <button className="yes" onClick={() => setYes(true)}>
+              Ja 💕
+            </button>
+
+            <button className="no">
+              Nein 🙈
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
